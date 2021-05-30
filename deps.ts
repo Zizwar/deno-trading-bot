16e6d8a97958bcca7cb156abe6cda99b0905ff82
Zizwar <zizwar@gmail.com>
2021-05-30 21:43:39 +0000

    upt v0.0.1

---
+++ deps.ts
@@ -0,0 +1,2 @@
+//export { Router,Response, RouteParams, Request ,Application} from "https://deno.land/x/oak/mod.ts";
+export * as technicalindicators from 'https://esm.sh/technicalindicators';
--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 //technicalindicators
-import {SMA} from 'https://esm.sh/technicalindicators'
-//deno run --allow-net --allow-env --allow-read .\app.ts
-console.info(SMA.calculate({period : 5, values : [1,2,3,4,5,6,7,8,9]}))
+import { technicalindicators } from './deps.ts';
+const {SMA} = technicalindicators;
+console.info(SMA.calculate({ period: 5, values: [1, 2, 3, 4, 5, 6, 7, 8, 9] }))
